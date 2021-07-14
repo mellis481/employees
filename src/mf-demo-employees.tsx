ff44fb7e0ff9c554d58a0ee4f2ed907d369f5926
Michael Ellis <michael.ellis@vertexinc.com>
2021-07-14 09:45:12 -0400

    Reverted changes

--- src/mf-demo-employees.tsx
+++ src/mf-demo-employees.tsx
@@ -2,36 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
-import root from "react-shadow";
 import "./set-public-path";
-
-const styles = require("!!to-string-loader!css-loader!./styles.css");
-
-// Reference to shadow DOM root in case you need it for 'portaled'
-// Example in date-picker-popover.tsx
-export let mfeWebComponent: React.RefObject<HTMLElement> | undefined;
-
-class WrappedRoot extends React.Component<any> {
-  constructor(props: any) {
-    super(props);
-    mfeWebComponent = React.createRef();
-  }
-  render() {
-    return (
-      <root.div ref={mfeWebComponent} mode="open">
-        <div className="mf-demo-employees">
-          <style type="text/css">{styles}</style>
-          <Root />
-        </div>
-      </root.div>
-    );
-  }
-}
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: WrappedRoot,
+  rootComponent: Root,
+  domElementGetter,
 });
 
 export const { bootstrap, mount, unmount } = lifecycles;
+
+function domElementGetter() {
+  let el = document.getElementById("mf-content");
+  if (!el) {
+    el = document.createElement("div");
+    el.id = "mf-demo-employees";
+    document.body.appendChild(el);
+  }
+  return el;
+}
